7fc00c46be637c976e0ca3c9a296b5bfdc5b51cc
longmaker2 <l.deng@alustudent.com>
2023-02-16 21:49:23 +0200

    files updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
