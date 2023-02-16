b36305f5cb186de0b0136206b1c19436e0ddcecc
longmaker2 <l.deng@alustudent.com>
2023-02-16 21:57:50 +0200

    files updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    puts(""Programming is like building a multilingual puzzle");
-    return (0);
+/** 
+ * main - Entry points
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+		return(0);
 }
