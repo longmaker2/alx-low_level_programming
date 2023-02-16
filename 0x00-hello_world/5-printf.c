c4e79ee1870e3c6e1b599eeb860bb490619ba7ac
longmaker2 <l.deng@alustudent.com>
2023-02-16 21:45:00 +0200

    files updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-    printf("With proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+		return(0);
 }
