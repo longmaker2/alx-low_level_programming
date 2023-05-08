93d18ac427a447d00f5b7ccb4cdbfb2ee790ff49
longmaker2 <l.deng@alustudent.com>
2023-05-08 15:00:15 +0200

    files updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,35 @@
 #include "main.h"
+
 /**
  * create_file - function that creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  * Return: If the function fails - -1 otherwise - 1.
- *//**
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- * Return: If the function fails - -1 otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+    int fd, len;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
+    if (fd == -1) {
+        if (errno == EEXIST) {
+            fd = open(filename, O_WRONLY | O_TRUNC);
+        }
+    }
+    if (fd == -1)
+        return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+    if (text_content != NULL) {
+        len = strlen(text_content);
+        if (write(fd, text_content, len) != len) {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+    close(fd);
+    return (1);
 }
