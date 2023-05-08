4555b4827f1fc8346d4860a3fbe678def2b5e71a
longmaker2 <l.deng@alustudent.com>
2023-05-08 15:02:30 +0200

    files updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- * Return: If the function fails - -1 otherwise - 1.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (filename == NULL)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
-    if (fd == -1) {
-        if (errno == EEXIST) {
-            fd = open(filename, O_WRONLY | O_TRUNC);
-        }
-    }
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (text_content != NULL) {
-        len = strlen(text_content);
-        if (write(fd, text_content, len) != len) {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (fd == -1)
+		return (-1);
 
-    close(fd);
-    return (1);
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
